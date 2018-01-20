744f5cc8239062707e0caff1116c218f819856eb
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-20 06:58:24 +0530

    Image loading working

--- app/src/main/java/rm/iamjosephvarghese/glidesample/MainActivity.java
+++ app/src/main/java/rm/iamjosephvarghese/glidesample/MainActivity.java
@@ -2,12 +2,26 @@ package rm.iamjosephvarghese.glidesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageView);
+
+
+        Glide.with(this).load("https://static.giantbomb.com/uploads/original/15/157771/2312719-a6.jpg").into(imageView);
+
+
     }
 }
